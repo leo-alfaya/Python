607ae602f5c3cdc6797c6f22d454dc76cd68f1d2
Leonardo Fonseca <leo.alfaya@gmail.com>
2017-10-25 23:50:48 -0200

    select_core added

--- Projetos/sqlalchemy_live_python/core_insert.py
+++ Projetos/sqlalchemy_live_python/core_insert.py
@@ -4,8 +4,15 @@ conn = engine.connect()
 
 ins = user_table.insert()
 
-new_user = ins.values(nome='Leonardo',
-					 idade=28,
-					 senha='1234')
+# new_user = ins.values(nome='Leonardo',
+# 					 idade=28,
+# 					 senha='1234')
 
-conn.execute(new_user)
+# conn.execute(new_user)
+
+conn.execute(user_table.insert(), [
+	{'nome': 'Marivaldo', 'idade': 35, 'senha': 'gatinho_123'},
+	{'nome': 'Jean', 'idade': 19, 'senha': 'jeanzinho_123'},
+	{'nome': 'Juancy', 'idade': 27, 'senha': 'souzapereira'},
+	{'nome': 'Luciana', 'idade': 25, 'senha': 'gatinha_luluzinha'}
+])
