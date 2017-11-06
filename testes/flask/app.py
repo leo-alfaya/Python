2434885060569804b3649ee7d5b201d4294bb288
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2017-11-06 00:06:17 -0200

    flask teste added

---
+++ testes/flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask("flask")
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World! <strong>I am learning Flask</strong>", 200
+
+
+app.run(debug=True, use_reloader=True)
