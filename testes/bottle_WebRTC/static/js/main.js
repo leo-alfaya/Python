18cf40b996a53fa0dea5f1459b6089a5ebdb4816
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2017-11-27 16:36:06 -0200

    Blueprints

--- testes/bottle_WebRTC/static/js/main.js
+++ testes/bottle_WebRTC/static/js/main.js
@@ -1,28 +1,6 @@
-'use strict';
+var users = [];
 
-navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-var constraints = {
-  audio: false,
-  video: true
-};
-var video_box = null;
-
-function successCallback(stream) {
-  window.stream = stream; // stream available to console  
-  if (window.URL) {
-    video_box.src = window.URL.createObjectURL(stream);
-  } else {
-    video_box.src = stream;
-  }
+function getNewId(){
+	return Math.ceil(Math.random() * (1000000000 - 0 + 1))
 }
 
-function errorCallback(error) {
-  console.log('navigator.getUserMedia error: ', error);
-}
-
-function getMedia(video_box){
-	video_box = document.querySelector(video_box);
-	navigator.getUserMedia(constraints, successCallback, errorCallback);
-}
---
+++ testes/flask/run.py
@@ -0,0 +1,4 @@
+from news_app import create_app
+
+app = create_app(config_filename='settings.py')
+app.run(debug=True, use_reloader=True)
